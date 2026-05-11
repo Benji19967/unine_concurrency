c27911b51668f6af0ccac92532f32c5e2315df31
Benjamin Labrecque <labrecque.benji@gmail.com>
2026-05-11 19:09:40 +0200

    refactor: extract pixelPainter logic to separate class

--- src/main/java/concurrency/mandel/Mandel.java
+++ src/main/java/concurrency/mandel/Mandel.java
@@ -16,13 +16,10 @@ import javax.swing.border.BevelBorder;
 import java.awt.BorderLayout;
 
 public final class Mandel extends JPanel implements MouseListener, MouseMotionListener, KeyListener, Runnable {
-    private int maxCount = 192; // maximum number of iterations
     private boolean smooth = false; // smoothing state
     private boolean antialias = false; // antialias state
     private boolean toDrag = false; // dragging state
     private boolean rect = true; // zooming or moving mode for dragging
-    private Color[][] colors; // palettes
-    private int pal = 0; // current palette
 
     // currently visible relative window dimensions
     private double viewX = 0.0;
@@ -41,19 +38,9 @@ public final class Mandel extends JPanel implements MouseListener, MouseMotionLi
     private int mouseX, mouseY; // mouse position when the button was pressed
     private int dragX, dragY; // current mouse position during dragging
 
-    private static final int[][][] colpal = { // palette colors
-            { { 12, 0, 10, 20 }, { 12, 50, 100, 240 }, { 12, 20, 3, 26 }, { 12, 230, 60, 20 },
-                    { 12, 25, 10, 9 }, { 12, 230, 170, 0 }, { 12, 20, 40, 10 }, { 12, 0, 100, 0 },
-                    { 12, 5, 10, 10 }, { 12, 210, 70, 30 }, { 12, 90, 0, 50 }, { 12, 180, 90, 120 },
-                    { 12, 0, 20, 40 }, { 12, 30, 70, 200 } },
-            { { 10, 70, 0, 20 }, { 10, 100, 0, 100 }, { 14, 255, 0, 0 }, { 10, 255, 200, 0 } },
-            { { 8, 40, 70, 10 }, { 9, 40, 170, 10 }, { 6, 100, 255, 70 }, { 8, 255, 255, 255 } },
-            { { 12, 0, 0, 64 }, { 12, 0, 0, 255 }, { 10, 0, 255, 255 }, { 12, 128, 255, 255 }, { 14, 64, 128, 255 } },
-            { { 16, 0, 0, 0 }, { 32, 255, 255, 255 } },
-    };
-
     private final int nThreads = 4;
     private final ExecutorService pool = Executors.newFixedThreadPool(nThreads);
+    private final PixelPainter pixelPainter = new PixelPainter(height, width);
 
     public void run() {
         while (thread != null) {
@@ -72,25 +59,7 @@ public final class Mandel extends JPanel implements MouseListener, MouseMotionLi
         addMouseListener(this);
         addMouseMotionListener(this);
         addKeyListener(this);
-        // initialize color palettes
-        colors = new Color[colpal.length][];
-        for (int p = 0; p < colpal.length; p++) { // process all palettes
-            int n = 0;
-            for (int i = 0; i < colpal[p].length; i++) // get the number of all colors
-                n += colpal[p][i][0];
-            colors[p] = new Color[n]; // allocate pallete
-            n = 0;
-            for (int i = 0; i < colpal[p].length; i++) { // interpolate all colors
-                int[] c1 = colpal[p][i]; // first referential color
-                int[] c2 = colpal[p][(i + 1) % colpal[p].length]; // second ref. color
-                for (int j = 0; j < c1[0]; j++) // linear interpolation of RGB values
-                    colors[p][n + j] = new Color(
-                            (c1[1] * (c1[0] - 1 - j) + c2[1] * j) / (c1[0] - 1),
-                            (c1[2] * (c1[0] - 1 - j) + c2[2] * j) / (c1[0] - 1),
-                            (c1[3] * (c1[0] - 1 - j) + c2[3] * j) / (c1[0] - 1));
-                n += c1[0];
-            }
-        }
+        pixelPainter.initColorPalettes();
         thread = null;
     }
 
@@ -118,13 +87,6 @@ public final class Mandel extends JPanel implements MouseListener, MouseMotionLi
         this.status.setText("Time=" + this.time + " ms");
     }
 
-    private static final int[][] rows = {
-            { 0, 16, 8 }, { 8, 16, 8 }, { 4, 16, 4 }, { 12, 16, 4 },
-            { 2, 16, 2 }, { 10, 16, 2 }, { 6, 16, 2 }, { 14, 16, 2 },
-            { 1, 16, 1 }, { 9, 16, 1 }, { 5, 16, 1 }, { 13, 16, 1 },
-            { 3, 16, 1 }, { 11, 16, 1 }, { 7, 16, 1 }, { 15, 16, 1 },
-    };
-
     private boolean draw() {
         Dimension size = getSize();
         // create offscreen buffer for double buffering
@@ -135,26 +97,17 @@ public final class Mandel extends JPanel implements MouseListener, MouseMotionLi
             graphics = image.getGraphics();
         }
 
-        // fractal image pre-drawing
-        for (int y = 0; y < height + 4; y += 8) {
-            if (Thread.interrupted())
-                return true;
-            for (int x = 0; x < width + 4; x += 8) {
-                Color color = getColorNormal(x, y);
-                graphics.setColor(color);
-                graphics.fillRect(x - 4, y - 4, 8, 8);
-            }
-        }
-        repaint();
+        pixelPainter.setWidthAndHeight(width, height);
+        pixelPainter.setViewsAndZoom(viewX, viewY, zoom);
 
-        this.time = System.currentTimeMillis();
         // fractal image drawing
+        this.time = System.currentTimeMillis();
         Color[][] pixels = new Color[height][width];
         for (int y = 0; y < height; y++) {
             if (Thread.interrupted())
                 return true;
             for (int x = 0; x < width; x++) {
-                Color color = getColor(x, y);
+                Color color = pixelPainter.getColor(x, y);
                 pixels[y][x] = color;
             }
         }
@@ -171,73 +124,6 @@ public final class Mandel extends JPanel implements MouseListener, MouseMotionLi
         this.updateStatus();
 
         return false;
-    }
-
-    private Color getColorNormal(int x, int y) {
-        double r = zoom / Math.min(width, height);
-        double dx = 2.5 * (x * r + viewX) - 2;
-        double dy = 1.25 - 2.5 * (y * r + viewY);
-        Color color = color(dx, dy);
-        return color;
-    }
-
-    // gets a color for a specific pixel
-    private Color getColor(int x, int y) {
-        double r = zoom / Math.min(width, height);
-        double dx = 2.5 * (x * r + viewX) - 2;
-        double dy = 1.25 - 2.5 * (y * r + viewY);
-        Color color = color(dx, dy);
-        // computation of average color for antialiasing
-        if (antialias) {
-            Color c1 = color(dx - 0.25 * r, dy - 0.25 * r);
-            Color c2 = color(dx + 0.25 * r, dy - 0.25 * r);
-            Color c3 = color(dx + 0.25 * r, dy + 0.25 * r);
-            Color c4 = color(dx - 0.25 * r, dy + 0.25 * r);
-            int red = (color.getRed() + c1.getRed() + c2.getRed() + c3.getRed() + c4.getRed()) / 5;
-            int green = (color.getGreen() + c1.getGreen() + c2.getGreen() + c3.getGreen() + c4.getGreen())
-                    / 5;
-            int blue = (color.getBlue() + c1.getBlue() + c2.getBlue() + c3.getBlue() + c4.getBlue()) / 5;
-            color = new Color(red, green, blue);
-        }
-        return color;
-    }
-
-    // Computes a color for a given point
-    private Color color(double x, double y) {
-        int count = mandel(0.0, 0.0, x, y);
-        int palSize = colors[pal].length;
-        Color color = colors[pal][count / 256 % palSize];
-        if (smooth) {
-            Color color2 = colors[pal][(count / 256 + palSize - 1) % palSize];
-            int k1 = count % 256;
-            int k2 = 255 - k1;
-            int red = (k1 * color.getRed() + k2 * color2.getRed()) / 255;
-            int green = (k1 * color.getGreen() + k2 * color2.getGreen()) / 255;
-            int blue = (k1 * color.getBlue() + k2 * color2.getBlue()) / 255;
-            color = new Color(red, green, blue);
-        }
-        return color;
-    }
-
-    // Computes a value for a given complex number
-    private int mandel(double zRe, double zIm, double pRe, double pIm) {
-        double zRe2 = zRe * zRe;
-        double zIm2 = zIm * zIm;
-        double zM2 = 0.0;
-        int count = 0;
-        while (zRe2 + zIm2 < 4.0 && count < maxCount) {
-            zM2 = zRe2 + zIm2;
-            zIm = 2.0 * zRe * zIm + pIm;
-            zRe = zRe2 - zIm2 + pRe;
-            zRe2 = zRe * zRe;
-            zIm2 = zIm * zIm;
-            count++;
-        }
-        if (count == 0 || count == maxCount)
-            return 0;
-        // transition smoothing
-        zM2 += 0.000000001;
-        return 256 * count + (int) (255.0 * Math.log(4 / zM2) / Math.log((zRe2 + zIm2) / zM2));
     }
 
     // To prevent background clearing for each paint()
@@ -302,7 +188,7 @@ public final class Mandel extends JPanel implements MouseListener, MouseMotionLi
             }
             redraw(); // recompute and repaint
         } else if ((e.getModifiers() & InputEvent.BUTTON3_MASK) != 0) { // RMB
-            maxCount += maxCount / 4; // increase the number of iterations by 1/4
+            pixelPainter.updateMaxCount();
             redraw(); // recompute and repaint
         }
     }
@@ -333,7 +219,6 @@ public final class Mandel extends JPanel implements MouseListener, MouseMotionLi
 
     public void keyPressed(KeyEvent e) {
         if (e.getKeyCode() == KeyEvent.VK_ESCAPE) { // init
-            maxCount = 192;
             viewX = viewY = 0.0;
             zoom = 1.0;
             redraw(); // recompute and repaint
@@ -348,7 +233,7 @@ public final class Mandel extends JPanel implements MouseListener, MouseMotionLi
             zoom *= 2.0;
             redraw(); // recompute and repaint
         } else if (e.getKeyCode() == KeyEvent.VK_P) { // next palette
-            pal = (pal + 1) % colors.length;
+            pixelPainter.nextPal();
             redraw(); // recompute and repaint
         } else if (e.getKeyCode() == KeyEvent.VK_S) { // smoothing
             smooth = !smooth;
---
+++ src/main/java/concurrency/mandel/PixelPainter.java
@@ -0,0 +1,141 @@
+package mandel;
+
+import java.awt.*;
+
+public class PixelPainter {
+    private int maxCount = 192; // maximum number of iterations
+    private boolean smooth = false;
+    private boolean antialias = false;
+
+    private Color[][] colors; // palettes
+    private int pal = 0; // current palette
+    private static final int[][][] colpal = { // palette colors
+            { { 12, 0, 10, 20 }, { 12, 50, 100, 240 }, { 12, 20, 3, 26 }, { 12, 230, 60, 20 },
+                    { 12, 25, 10, 9 }, { 12, 230, 170, 0 }, { 12, 20, 40, 10 }, { 12, 0, 100, 0 },
+                    { 12, 5, 10, 10 }, { 12, 210, 70, 30 }, { 12, 90, 0, 50 }, { 12, 180, 90, 120 },
+                    { 12, 0, 20, 40 }, { 12, 30, 70, 200 } },
+            { { 10, 70, 0, 20 }, { 10, 100, 0, 100 }, { 14, 255, 0, 0 }, { 10, 255, 200, 0 } },
+            { { 8, 40, 70, 10 }, { 9, 40, 170, 10 }, { 6, 100, 255, 70 }, { 8, 255, 255, 255 } },
+            { { 12, 0, 0, 64 }, { 12, 0, 0, 255 }, { 10, 0, 255, 255 }, { 12, 128, 255, 255 }, { 14, 64, 128, 255 } },
+            { { 16, 0, 0, 0 }, { 32, 255, 255, 255 } },
+    };
+
+    private int width, height; // current screen width and height
+
+    // currently visible relative window dimensions
+    private double viewX = 0.0;
+    private double viewY = 0.0;
+    private double zoom = 1.0;
+
+    public PixelPainter(int height, int width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    public void nextPal() {
+        pal = (pal + 1) % colors.length;
+    }
+
+    public void updateMaxCount() {
+        maxCount += maxCount / 4; // increase the number of iterations by 1/4
+    }
+
+    public void setViewsAndZoom(double viewX, double viewY, double zoom) {
+        this.viewX = viewX;
+        this.viewY = viewY;
+        this.zoom = zoom;
+    }
+
+    public void setWidthAndHeight(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public void initColorPalettes() {
+        colors = new Color[colpal.length][];
+        for (int p = 0; p < colpal.length; p++) { // process all palettes
+            int n = 0;
+            for (int i = 0; i < colpal[p].length; i++) // get the number of all colors
+                n += colpal[p][i][0];
+            colors[p] = new Color[n]; // allocate pallete
+            n = 0;
+            for (int i = 0; i < colpal[p].length; i++) { // interpolate all colors
+                int[] c1 = colpal[p][i]; // first referential color
+                int[] c2 = colpal[p][(i + 1) % colpal[p].length]; // second ref. color
+                for (int j = 0; j < c1[0]; j++) // linear interpolation of RGB values
+                    colors[p][n + j] = new Color(
+                            (c1[1] * (c1[0] - 1 - j) + c2[1] * j) / (c1[0] - 1),
+                            (c1[2] * (c1[0] - 1 - j) + c2[2] * j) / (c1[0] - 1),
+                            (c1[3] * (c1[0] - 1 - j) + c2[3] * j) / (c1[0] - 1));
+                n += c1[0];
+            }
+        }
+    }
+
+    // gets a color for a specific pixel
+    public Color getColor(int x, int y) {
+        double r = zoom / Math.min(width, height);
+        double dx = 2.5 * (x * r + viewX) - 2;
+        double dy = 1.25 - 2.5 * (y * r + viewY);
+        Color color = color(dx, dy);
+        // computation of average color for antialiasing
+        if (antialias) {
+            Color c1 = color(dx - 0.25 * r, dy - 0.25 * r);
+            Color c2 = color(dx + 0.25 * r, dy - 0.25 * r);
+            Color c3 = color(dx + 0.25 * r, dy + 0.25 * r);
+            Color c4 = color(dx - 0.25 * r, dy + 0.25 * r);
+            int red = (color.getRed() + c1.getRed() + c2.getRed() + c3.getRed() + c4.getRed()) / 5;
+            int green = (color.getGreen() + c1.getGreen() + c2.getGreen() + c3.getGreen() + c4.getGreen())
+                    / 5;
+            int blue = (color.getBlue() + c1.getBlue() + c2.getBlue() + c3.getBlue() + c4.getBlue()) / 5;
+            color = new Color(red, green, blue);
+        }
+        return color;
+    }
+
+    public Color getColorNormal(int x, int y) {
+        double r = zoom / Math.min(width, height);
+        double dx = 2.5 * (x * r + viewX) - 2;
+        double dy = 1.25 - 2.5 * (y * r + viewY);
+        Color color = color(dx, dy);
+        return color;
+    }
+
+    // Computes a color for a given point
+    private Color color(double x, double y) {
+        int count = mandel(0.0, 0.0, x, y);
+        int palSize = colors[pal].length;
+        Color color = colors[pal][count / 256 % palSize];
+        if (smooth) {
+            Color color2 = colors[pal][(count / 256 + palSize - 1) % palSize];
+            int k1 = count % 256;
+            int k2 = 255 - k1;
+            int red = (k1 * color.getRed() + k2 * color2.getRed()) / 255;
+            int green = (k1 * color.getGreen() + k2 * color2.getGreen()) / 255;
+            int blue = (k1 * color.getBlue() + k2 * color2.getBlue()) / 255;
+            color = new Color(red, green, blue);
+        }
+        return color;
+    }
+
+    // Computes a value for a given complex number
+    private int mandel(double zRe, double zIm, double pRe, double pIm) {
+        double zRe2 = zRe * zRe;
+        double zIm2 = zIm * zIm;
+        double zM2 = 0.0;
+        int count = 0;
+        while (zRe2 + zIm2 < 4.0 && count < maxCount) {
+            zM2 = zRe2 + zIm2;
+            zIm = 2.0 * zRe * zIm + pIm;
+            zRe = zRe2 - zIm2 + pRe;
+            zRe2 = zRe * zRe;
+            zIm2 = zIm * zIm;
+            count++;
+        }
+        if (count == 0 || count == maxCount)
+            return 0;
+        // transition smoothing
+        zM2 += 0.000000001;
+        return 256 * count + (int) (255.0 * Math.log(4 / zM2) / Math.log((zRe2 + zIm2) / zM2));
+    }
+}
